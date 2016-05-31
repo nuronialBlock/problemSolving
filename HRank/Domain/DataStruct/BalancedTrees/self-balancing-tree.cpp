c89dfcff7cf7c44a7933764778c599df0436ff2c
nuronial_block <ahnaf@furqansoftware.com>
2016-06-01 03:00:22 +0600

    Solve AVL tree problem

--- HRank/Domain/DataStruct/BalancedTrees/self-balancing-tree.cpp
+++ HRank/Domain/DataStruct/BalancedTrees/self-balancing-tree.cpp
@@ -7,28 +7,107 @@ typedef struct node
     int ht;
 } node; */
 
-node* findImbalanceNode(node* root, int val){
-	if(root->left == NULL && root->right == NULL){
-		node *newNode = new node; 
-		newNode->val = val;
-		newNode->ht = 1;
-		if(val >= root->val) root->right = newNode;
-		else root->left = newNode;
-		return newNode;
+
+
+void setHeight(node* root){
+	if(!root) return;
+	int left = -1;
+	int right= -1;
+	if(root->left)
+		left = root->left->ht;
+	if (root->right)
+		right = root->right->ht;
+
+	root->ht = 1 + max(left, right);
+}
+
+int bf(node* root){
+	if(!root) return 0;
+	int left = -1;
+	int right= -1;
+	if (root->left)
+		left = root->left->ht;
+	if (root->right)
+		right = root->right->ht;
+
+	return left - right;
+}
+
+void rotateLL(node* root){
+	node* l = root->left;
+	node* r = root->right;
+	node* ll = root->left->left;
+	node* lr = root->left->right;
+
+	root->left = ll;
+	root->right = l;
+	l->left = lr;
+	l->right=r;
+
+	int swap = root->val;
+	root->val =  l->val;
+	l->val = swap;
+
+	setHeight(l);
+	setHeight(root);
+}
+
+void rotateRR(node* root){
+	node* r  = root->right;
+	node* rr = root->right->right;
+	node* rl = root->right->left;
+	node* l = root->left;
+
+	root->right = rr;
+	root->left = r;
+	r->right = rl;
+	r->left = l;
+
+	int swap = root->val;
+	root->val = r->val;
+	r->val = swap;
+
+	setHeight(r);
+	setHeight(root);
+}
+
+
+void rotateLR(node* root){
+	rotateRR(root->left);
+	rotateLL(root);
+}
+
+
+void rotateRL(node* root){
+	rotateLL(root->right);
+	rotateRR(root);
+}
+
+void makeBalance(node* root){
+	if(!root) return;
+	if(bf(root) == 2){
+		if(bf(root->left) == 1) rotateLL(root);
+		else if(bf(root->left) == -1) rotateLR(root);
 	}
-
-	node *n;
-
-	if(val >= root->val) n = findImbalanceNode(root->right, val);
-	else n = findImbalanceNode(root->left, val);
-
-	int hi = n->left->ht - n->right->ht;
-	if(hi < -1 || hi > 1) return n;
-
-
+	else if(bf(root) == -2){
+		if(bf(root->right) == -1) rotateRR(root);
+		else if(bf(root->right) == 1) rotateRL(root);
+	}
 }
 
 node* insert(node* root,int val) {
-	node* tmp = root;
-	node* imbNode = findImbalanceNode(root, val);
+	if(!root){
+		node* newNode = new node;
+		newNode->val = val;
+		newNode->left = NULL; 
+		newNode->right = NULL;
+		root = newNode; 
+	}
+	else if(val < root->val) root->left = insert(root->left, val);
+	else root->right = insert(root->right, val);
+
+	setHeight(root);
+	makeBalance(root);
+
+	return root;
 }
